8e5b82f226c1aeed76d5d6ac604c2f8df630c350
ariztegui8 <jorgeariztegui8@gmail.com>
2024-08-07 20:47:57 -0300

    aplicando checkout

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div className='py-20'>
+        <p className='text-xl font-medium'>CARRITO DE COMPRAS</p>
+    </div>
+  )
+}
+
+export default Checkout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-
-import type { Metadata } from "next";
+'use client'
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { usePathname } from "next/navigation";
+import type { Metadata } from "next";
 
-const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Veré Clothing",
   description: "Veré Clothing Store",
 };
+
+const inter = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -18,14 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
+  const pathname = usePathname();
+  const isCheckout = pathname === '/checkout';
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        {!isCheckout && <Navbar />}
         {children}
-        <div className='bg-[#303030] py-8 px-4 md:px-6 xl:px-12'>
-          <Footer />
-        </div>
+        {!isCheckout && (
+          <div className='bg-[#303030] py-8 px-4 md:px-6 xl:px-12'>
+            <Footer />
+          </div>
+        )}
       </body>
     </html>
   );
